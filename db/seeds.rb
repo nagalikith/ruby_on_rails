ea82487a83a537b4f3e3ce1002ca878902396dec
James Yeung <jyeung2@sheffield.ac.uk>
2021-04-05 19:16:23 +0100

    Started login

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,9 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+User.where(email:
+'bgsyc@sheffield.ac.uk').first_or_create(password:
+'southyork', password_confirmation: 'southyork', club_id: '1')
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
