40ee73c0947d613b695b4b2c73a1147a0520aa5e
Alex Parr <ajparr1@sheffield.ac.uk>
2021-04-05 11:49:44 +0100

    Syntax changes

--- db/migrate/20210329210511_create_clubs.rb
+++ db/migrate/20210329210511_create_clubs.rb
@@ -1,6 +1,6 @@
 class CreateClubs < ActiveRecord::Migration[6.0]
   def change
-    create_table :clubs, primary_key :club_id do |t|
+    create_table :clubs, primary_key: 'club_id' do |t|
       t.string :name
       t.string :postcode
       t.integer :contactnumber
--- db/migrate/20210329210631_create_donations.rb
+++ db/migrate/20210329210631_create_donations.rb
@@ -1,6 +1,6 @@
 class CreateDonations < ActiveRecord::Migration[6.0]
   def change
-    create_table :donations, primary_key :donation_id do |t|
+    create_table :donations, primary_key: 'donation_id' do |t|
       t.float :amount
       t.date :date
       t.string :method
