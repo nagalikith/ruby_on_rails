711659350740aa7c3816ff23bedf72270fe14ea7
Alex Parr <ajparr1@sheffield.ac.uk>
2021-04-24 00:35:42 +0100

    Bug Fixes

--- app/models/meeting_type.rb
+++ app/models/meeting_type.rb
@@ -22,9 +22,15 @@ class MeetingType < ApplicationRecord
     belongs_to :club_info
 
     def mostRecentMeetings(club_id)
-        most_recent_club_info_id = ClubInfo.select(:id).where(club_id: club_id).order("date DESC").first.id
-        meeting_array = MeetingType.where(club_info_id: most_recent_club_info_id)
-        return meeting_array
+        most_recent_club_info_id_array = ClubInfo.select(:id).where(club_id: club_id).order("date DESC")
+        if most_recent_club_info_id_array.length > 0
+            most_recent_club_info_id = most_recent_club_info_id_array.first.id
+        
+            meeting_array = MeetingType.where(club_info_id: most_recent_club_info_id)
+            return meeting_array
+        else
+            return []
+        end
     end
 
 end
