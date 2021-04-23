50628cc1e5cd98e8ebb6b70ab685eedd17e34450
Alex Parr <ajparr1@sheffield.ac.uk>
2021-04-23 07:29:09 +0100

    Seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
 
-#User.create(email:
-#'bgsyc@sheffield.ac.uk', password:
-#'southyork', password_confirmation: 'southyork', club_id: '1', manager: true)
-#Club.where(name: 'test club')
 User.delete_all
 Club.delete_all
 Volunteer.delete_all
@@ -34,11 +29,16 @@ Volunteer.where(name: "Josh").first_or_create(contactnumber: '07264893487', emai
 Volunteer.where(name: "Ellie").first_or_create(contactnumber: '07125648376', email: 'ellie@email.com', hours: '32', target: '20', youngPerson: false)
 Volunteer.where(name: "Adam").first_or_create(contactnumber: '07898562736', email: 'adam@email.com', hours: '0', target: '10', youngPerson: false)
 
+Event.where(name: "Bike Ride").first_or_create(club_id: "5", date:"2021/04/20", comment: "Bring your own bike")
+Event.where(name: "Walk").first_or_create(club_id: "3", date:"2021/05/01", comment: "A 4 mile walk")
+Event.where(name: "Swimming Trip").first_or_create(club_id: "4", date:"2021/05/12", comment: "A trip to a local swimming pool")
 
-#User.where(email:
-#'bgsyc@sheffield.ac.uk').first_or_create(password:
-#'southyork', password_confirmation: 'southyork', club_id: '1', manager: true)
+EventFeedback.where(id: '1').first_or_create(comment: "Was an amazing event. Well organised", event_id: '1')
+EventFeedback.where(id: '2').first_or_create(comment: "Very fun", event_id: '1')
 
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+CaseStudy.where(id: "1").first_or_create(participant: "Molly", date: "2021/03/02", comment: "Very well behaved", club_id: "2")
+CaseStudy.where(id: "2").first_or_create(participant: "Ben", date: "2021/01/20", comment: "Organised a game", club_id: "2")
+CaseStudy.where(id: "3").first_or_create(participant: "Chris", date: "2021/02/16", comment: "Tried very hard, put in 110%", club_id: "4")
+
+
 
