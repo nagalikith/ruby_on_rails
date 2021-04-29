78a77d0138afb4c4a7fe66119a047c2194eb8f8e
James Yeung <jyeung2@sheffield.ac.uk>
2021-04-29 01:41:09 +0100

    Edit events through admin works

--- app/models/event.rb
+++ app/models/event.rb
@@ -25,7 +25,7 @@ class Event < ApplicationRecord
     has_many :event_feedback , dependent: :destroy
     belongs_to :club
     has_many :consent_form , dependent: :destroy
-    has_many :case_study , dependent: :destroy
+    # has_many :case_study , dependent: :destroy
     belongs_to :club
     #belongs_to :event_calendars
     
