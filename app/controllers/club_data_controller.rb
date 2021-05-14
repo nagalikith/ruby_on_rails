d92a1b83c842ce1b2f7bd00db0e1e3fa4e6f2c3d
Naga Likith <nlsurapaneni1@sheffield.ac.uk>
2021-05-14 13:19:01 +0530

    Fixed PDF

--- app/controllers/club_data_controller.rb
+++ app/controllers/club_data_controller.rb
@@ -1,18 +1,15 @@
 class ClubDataController < ApplicationController
   before_action :set_club_datum, only: [:show, :edit]
-
   # GET /club_data
   def index
-    @club_data_array = []
     @club_data = Club.all
     @club_infos = ClubInfo.all
-    @sum = sum
-    @average = average
     club_data_array
+    puts("INDEX CONTROLLER #{@club_data_array}")
     respond_to do |format|
       format.html
       format.pdf do 
-        pdf = DataPdf.new(@club_data_array)
+        pdf = DataPdf.new(club_data_array())
         send_data pdf.render , filename: "Club_Data.pdf", type: "application/pdf"
       end
     end
@@ -20,12 +17,23 @@ class ClubDataController < ApplicationController
 
   # GET /club_data/1
   def show
+    respond_to do |format|
+      format.html
+      format.pdf do 
+        pdf = DataPdf.new(club_data_array())
+        send_data pdf.render , filename: "Club_Data.pdf", type: "application/pdf"
+      end
+    end
   end
 
   # POST /club_data/search
   def search
 #   {"search" => {"name" => "some entered name"} }
-    @club_data = Club.where(postcode: params[:search][:postcode])
+    $parameter = params[:search][:postcode]
+    @club_data = Club.all.where("postcode LIKE :search", search: "%#{$parameter}%")
+    @club_infos = ClubInfo.all
+    club_data_array
+    puts("Search Data #{@club_data_array}")
     render :index
   end
 
@@ -44,46 +52,58 @@ end
 
 def sum
   total = [0,0,0,0,0,0,0,0,0]
-  @club_infos.each do |club_info|
-    total[0] += club_info.males
-    total[1] +=club_info.females
-    total[2] +=club_info.lowerage
-    total[3] +=club_info.upperage
-    total[4] +=club_info.disability
-    total[5] +=club_info.ethnicity
-    total[6] +=club_info.depravation
-    total[7] +=club_info.drugsandabs
-    total[8] +=club_info.neets
+  @club_data.each do |club_datum|
+    @club_infos.each do |club_info|
+      if club_info.club_id == club_datum.id then
+        total[0] += club_info.males
+        total[1] +=club_info.females
+        total[2] +=club_info.lowerage
+        total[3] +=club_info.upperage
+        total[4] +=club_info.disability
+        total[5] +=club_info.ethnicity
+        total[6] +=club_info.depravation
+        total[7] +=club_info.drugsandabs
+        total[8] +=club_info.neets
+      end
+    end
   end
   return total
 end
 
-def average
+def average(total)
   avg = []
-  @sum.each do |num|
-    avg.push( num / (@club_infos.length))
+  if total > 0 then
+    @sum.each do |num|
+      avg.push( num / (total))
+    end
   end
   return avg
 end
 
 def club_data_array
-    @club_data.each do |club_datum|
-      @club_infos.each do |club_info|
-        if club_info.club_id == club_datum.id then
-          @club_data_array.push([club_datum.name,
-          club_datum.postcode, 
-          club_info.males,
-          club_info.females,
-          club_info.lowerage,
-          club_info.upperage,
-          club_info.disability,
-          club_info.ethnicity,
-          club_info.depravation,
-          club_info.drugsandabs,
-          club_info.neets])
-        end
+  @club_data_array = []
+  count = 0  
+  @club_data = Club.all.where("postcode LIKE :search", search: "%#{$parameter}%")
+  @club_data.each do |club_datum|
+    @club_infos.each do |club_info|
+      if club_info.club_id == club_datum.id then
+        count += 1
+        @club_data_array.push([club_datum.name,
+        club_datum.postcode, 
+        club_info.males,
+        club_info.females,
+        club_info.lowerage,
+        club_info.upperage,
+        club_info.disability,
+        club_info.ethnicity,
+        club_info.depravation,
+        club_info.drugsandabs,
+        club_info.neets])
       end
     end
+  end
+  @sum = sum
+  @average = average(count)
   @club_data_array.push(["SUM"] + [" "] + @sum)
   @club_data_array.push(["AVERAGE"] + [" "] + @average)         
 end
